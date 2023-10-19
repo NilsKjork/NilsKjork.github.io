e041feb5fbcb7214c5d6e1932b4407eab94ff4e6
NilsKjork <148435665+NilsKjork@users.noreply.github.com>
2023-10-19 10:37:48 +0200

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,13 @@
+var btn = document.getElementById("myBtn");
+
+//btn.addEventListener("click", playvideo);
+
+
+
+alert("dwada");
+
+function playvideo() {
+    let vid = document.getElementById("atcafe.mp4");
+    vid.play();
+
+}
